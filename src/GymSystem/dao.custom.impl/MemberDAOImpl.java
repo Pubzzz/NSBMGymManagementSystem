9dc57204dee885db819390d421697b62fc06b6b1
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-11-10 11:54:34 +0530

    Update MemberDAOImpl.java

--- src/GymSystem/dao.custom.impl/MemberDAOImpl.java
+++ src/GymSystem/dao.custom.impl/MemberDAOImpl.java
@@ -1,8 +1,7 @@
 package GymSystem.dao.custom.impl;
 
-import GymSystem.dao.DAOFactory;
+import GymSystem.dao.CrudUtil;
 import GymSystem.dao.custom.MemberDAO;
-import GymSystem.dto.MemberDTO;
 import GymSystem.entity.Member;
 
 import java.sql.ResultSet;
@@ -18,12 +17,12 @@ public class MemberDAOImpl implements MemberDAO{
     @Override
     public boolean add(Member ref) throws ClassNotFoundException, SQLException {
 
-        return CrudUtil.executeUpdate("Insert into Customer values(?,?,?,?,?)", ref.getId(), ref.getName(), ref.getAddress(), ref.getEmail(),ref.getTel());
+       // return CrudUtil.executeUpdate("Insert into Customer values(?,?,?,?,?)", ref.getId(), ref.getName(), ref.getAddress(), ref.getEmail(),ref.getTel());
     }
 
     @Override
     public boolean update(Member ref) throws ClassNotFoundException, SQLException {
-        return CrudUtil.executeUpdate("update Customer set customerName=?,customerAddress=?,customerEmail=? ,customerTel=? where customerId=?",  ref.getName(), ref.getAddress(), ref.getEmail(),ref.getTel(),ref.getId());
+        //return CrudUtil.executeUpdate("update Customer set customerName=?,customerAddress=?,customerEmail=? ,customerTel=? where customerId=?",  ref.getName(), ref.getAddress(), ref.getEmail(),ref.getTel(),ref.getId());
     }
 
     @Override
@@ -33,38 +32,38 @@ public class MemberDAOImpl implements MemberDAO{
 
     @Override
     public Member search1(String id) throws ClassNotFoundException, SQLException {
-        ResultSet rst = CrudUtil.executeQuery("select * from Customer where CustomerId=?", id);
-        if (rst.next()){
-            return new Customer(
-                    rst.getString(1),
-                    rst.getString(2),
-                    rst.getString(3),
-                    rst.getString(4),
-                    rst.getString(5)
-            );
-        }
+//        ResultSet rst = CrudUtil.executeQuery("select * from Customer where CustomerId=?", id);
+//        if (rst.next()){
+//            return new Member(
+//                    rst.getString(1),
+//                    rst.getString(2),
+//                    rst.getString(3),
+//                    rst.getString(4),
+//                    rst.getString(5)
+//            );
+//        }
         return null;
     }
 
     @Override
-    public Customer search2(String t) throws ClassNotFoundException, SQLException {
+    public Member search2(String t) throws ClassNotFoundException, SQLException {
         return null;
     }
 
 
     @Override
-    public ArrayList<Customer> getAll() throws Exception {
-        ArrayList<Customer> company = new ArrayList<>();
-        ResultSet rst = CrudUtil.executeQuery("Select * from customer");
-        while (rst.next()){
-            company.add(new Customer(
-                    rst.getString(1),
-                    rst.getString(2),
-                    rst.getString(3),
-                    rst.getString(4),
-                    rst.getString(5)
-            ));
-        }
-        return company;
+    public ArrayList<Member> getAll() throws Exception {
+//        ArrayList<Member> company = new ArrayList<>();
+//        ResultSet rst = CrudUtil.executeQuery("Select * from customer");
+//        while (rst.next()){
+//            company.add(new Member(
+//                    rst.getString(1),
+//                    rst.getString(2),
+//                    rst.getString(3),
+//                    rst.getString(4),
+//                    rst.getString(5)
+//            ));
+//        }
+//        return company;
     }
 }
