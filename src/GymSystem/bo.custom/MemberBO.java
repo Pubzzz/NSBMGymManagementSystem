ac93456e623ad388e9cf67f0148df26e2fd7b7e5
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-11-10 11:53:34 +0530

    Update MemberBO.java

--- src/GymSystem/bo.custom/MemberBO.java
+++ src/GymSystem/bo.custom/MemberBO.java
@@ -1,11 +1,13 @@
 package GymSystem.bo.custom;
 
+import GymSystem.bo.SuperBO;
 import GymSystem.dto.MemberDTO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface MemberBO {
+public interface MemberBO extends SuperBO {
+
     public  boolean addCustomer(MemberDTO ref) throws ClassNotFoundException, SQLException;
 
     public  boolean updateCustomer(MemberDTO ref) throws SQLException, ClassNotFoundException ;
