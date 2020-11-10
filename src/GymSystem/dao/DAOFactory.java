08f7c84ce247fa62808912ac840567d35eb06d91
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-11-10 11:54:09 +0530

    Update DAOFactory.java

--- src/GymSystem/dao/DAOFactory.java
+++ src/GymSystem/dao/DAOFactory.java
@@ -26,16 +26,11 @@ public class DAOFactory {
             case MEMBER:
                 return new MemberDAOImpl();
             case ATTENDANCE:
-                return new AttendanceDAOImpl();
+               // return new AttendanceDAOImpl();
             case ACCESSORIES:
-                return new AccessoriesDAOImpl();
+              //  return new AccessoriesDAOImpl();
             case INSTRUCTOR:
-                return new InstructorDAOImpl();
-
-
-
-
-
+              //  return new InstructorDAOImpl();
 
 
 
