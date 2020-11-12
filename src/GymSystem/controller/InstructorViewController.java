7e2772fc2aca728e8062c35fdb940b515f5f1cf2
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-11-12 17:15:23 +0530

    Upload image

---
+++ src/GymSystem/controller/InstructorViewController.java
@@ -0,0 +1,222 @@
+package GymSystem.controller;
+
+import GymSystem.bo.BOFactory;
+import GymSystem.bo.custom.InstructorBO;
+import GymSystem.dto.InstructorDTO;
+import GymSystem.dto.MemberDTO;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class InstructorViewController {
+
+    @FXML
+    private JFXButton btn_Register;
+
+    @FXML
+    private JFXButton btn_Update;
+
+    @FXML
+    private JFXButton btn_Remove;
+
+    @FXML
+    private JFXButton btn_Clear;
+
+    @FXML
+    private JFXTextField txt_InstructorId;
+
+    @FXML
+    private JFXTextField txt_InstructorName;
+
+    @FXML
+    private JFXTextField txt_InstructorNIC;
+
+    @FXML
+    private JFXTextField txt_InstructorEmail;
+
+    @FXML
+    private JFXTextField txt_InstructorTelNo;
+
+    @FXML
+    private JFXTextField txt_InstructorSearch;
+
+    @FXML
+    private JFXRadioButton radio_Male;
+
+    @FXML
+    private JFXRadioButton radio_Female;
+
+    @FXML
+    private TableView table_Instructor;
+
+
+    static InstructorBO bo = (InstructorBO) BOFactory.getInstance().getBO(BOFactory.BOTypes.INSTRUCTOR);
+
+    public static boolean addInstructor(InstructorDTO ref) throws ClassNotFoundException, SQLException {
+        return bo.addInstructor(ref);
+    }
+
+    public static boolean updateInstructor(InstructorDTO ref) throws SQLException, ClassNotFoundException {
+        return bo.updateInstructor(ref);
+    }
+
+    public static boolean deleteInstructor(String id) throws SQLException, ClassNotFoundException {
+        return bo.deleteInstructor(id);
+    }
+
+
+    public static InstructorDTO searchInstructor(String id) throws SQLException, ClassNotFoundException {
+        return bo.searchInstructor(id);
+    }
+
+
+
+    public void onaction_register(ActionEvent actionEvent) throws Exception {
+
+        String id=txt_InstructorId.getText();
+        String name=txt_InstructorName.getText();
+        String nic=txt_InstructorNIC.getText();
+        String gender;
+        if (radio_Male.isSelected()) {
+            gender = "Male";
+        } else {
+            gender = "Female";
+        }
+        String email=txt_InstructorEmail.getText();
+        String tp=txt_InstructorTelNo.getText();
+
+
+        InstructorDTO cusModel = new InstructorDTO(id, name, nic, gender, email,tp);
+        boolean addCustomer = InstructorViewController.addInstructor(cusModel);
+
+        if(addCustomer){
+            Alert a = new Alert(Alert.AlertType.INFORMATION, "ADDED SUCCESSFULLY ", ButtonType.OK);
+            a.showAndWait();
+            setAllClear();
+            getAllInstructors();
+
+
+
+        }else{
+            Alert a = new Alert(Alert.AlertType.WARNING, "FAILED ", ButtonType.OK);
+            a.showAndWait();
+        }
+
+    }
+
+    public void onaction_update(ActionEvent actionEvent) throws Exception {
+
+        String id=txt_InstructorId.getText();
+        String name=txt_InstructorName.getText();
+        String nic=txt_InstructorNIC.getText();
+        String gender;
+        if (radio_Male.isSelected()) {
+            gender = "Male";
+        } else {
+            gender = "Female";
+        }
+        String email=txt_InstructorEmail.getText();
+        String tp=txt_InstructorTelNo.getText();
+
+        InstructorDTO cusModel = new InstructorDTO(id, name, nic, gender, email, tp);
+        boolean updateInstructor = InstructorViewController.updateInstructor(cusModel);
+
+
+        if(updateInstructor){
+            Alert a = new Alert(Alert.AlertType.INFORMATION, "UPDATED SUCCESSFULLY", ButtonType.OK);
+            a.showAndWait();
+            setAllClear();
+            getAllInstructors();
+        }else{
+            Alert a = new Alert(Alert.AlertType.WARNING, "FAILED ", ButtonType.OK);
+            a.showAndWait();
+        }
+    }
+
+    public void onaction_remove(ActionEvent actionEvent) throws Exception {
+        String id = txt_InstructorId.getText();
+
+        boolean removeCustomer = deleteInstructor(id);
+        if(removeCustomer){
+            Alert a = new Alert(Alert.AlertType.INFORMATION, "DELETED SUCCSESSFULLY ", ButtonType.OK);
+            a.showAndWait();
+            setAllClear();
+            getAllInstructors();
+        }else{
+            Alert a = new Alert(Alert.AlertType.WARNING, "FAILED ", ButtonType.OK);
+            a.showAndWait();
+        }
+    }
+
+    public void onaction_clear(ActionEvent actionEvent) {
+        setAllClear();
+    }
+
+    public void onaction_search(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+
+        InstructorDTO searchInstructor = bo.searchInstructor(txt_InstructorSearch.getText());
+
+        txt_InstructorId.setText(searchInstructor.getId());
+        txt_InstructorName.setText(searchInstructor.getName());
+        txt_InstructorNIC.setText(searchInstructor.getNic());
+        // how to update on gender
+        txt_InstructorEmail.setText(searchInstructor.getEmail());
+        txt_InstructorTelNo.setText(searchInstructor.getTp());
+    }
+
+    public void onaction_clicked(MouseEvent mouseEvent) {
+
+//        InstructorDTO selectedItem = table_Instructor.getSelectionModel().getSelectedItem();
+//
+//        txt_InstructorId.setText(selectedItem.getId());
+//        txt_InstructorName.setText(selectedItem.getName());
+//        txt_InstructorNIC.setText(selectedItem.getNic());
+//        // how to update on gender
+//        txt_InstructorEmail.setText(selectedItem.getEmail());
+//        txt_InstructorTelNo.setText(selectedItem.getTp());
+    }
+
+
+    private void setAllClear(){
+        txt_InstructorId.clear();
+        txt_InstructorName.clear();
+        txt_InstructorNIC.clear();
+//        txt_InsEmail.clear();
+        txt_InstructorEmail.clear();
+        txt_InstructorTelNo.clear();
+
+
+
+    }
+
+    private void getAllInstructors() throws Exception {
+        ArrayList<InstructorDTO> instructorList;
+        try {
+//            instructorList = bo.getAllInstructor();
+//            ObservableList<InstructorDTO> instructor = FXCollections.observableArrayList(instructorList);
+//            table_Instructor.setItems(instructor);
+//            table_Instructor.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+//            table_Instructor.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+//            table_Instructor.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("nic"));
+//            table_Instructor.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("gender"));
+//            table_Instructor.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("email"));
+//            table_Instructor.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("tp"));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
