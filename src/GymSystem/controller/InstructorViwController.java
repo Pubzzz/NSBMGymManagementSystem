34fbc5c264933e6900eeaf6594229c4474f2a175
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-11-12 09:31:15 +0530

    Update controller.java

--- src/GymSystem/controller/InstructorViwController.java
+++ src/GymSystem/controller/InstructorViwController.java
@@ -1,9 +1,56 @@
 package GymSystem.controller;
 
+import GymSystem.dto.MemberDTO;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 
 public class InstructorViwController {
+
+    @FXML
+    private JFXButton btn_Register;
+
+    @FXML
+    private JFXButton btn_Update;
+
+    @FXML
+    private JFXButton btn_Remove;
+
+    @FXML
+    private JFXButton btn_Clear;
+
+    @FXML
+    private JFXTextField txt_InstructorId;
+
+    @FXML
+    private JFXTextField txt_InstructorName;
+
+    @FXML
+    private JFXTextField txt_InstructorNIC;
+
+    @FXML
+    private JFXTextField txt_InstructorEmail;
+
+    @FXML
+    private JFXTextField txt_InstructorTelNo;
+
+    @FXML
+    private JFXTextField txt_InstructorSearch;
+
+    @FXML
+    private JFXRadioButton radio_Male;
+
+    @FXML
+    private JFXRadioButton radio_Female;
+
+    @FXML
+    private TableView table_Instructor;
+
+
 
     public void onaction_register(ActionEvent actionEvent) {
     }
