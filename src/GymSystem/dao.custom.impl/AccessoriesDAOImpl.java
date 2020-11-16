21826eb7f6183cf7906d77989792cec8fd6003bd
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2020-11-16 23:15:06 +0530

    Update AccessoriesDAOImpl.java

--- src/GymSystem/dao.custom.impl/AccessoriesDAOImpl.java
+++ src/GymSystem/dao.custom.impl/AccessoriesDAOImpl.java
@@ -1,8 +1,72 @@
-package GymSystem.dao.custom;
+package GymSystem.dao.custom.impl;
 
-import GymSystem.dao.CrudDAO;
+import GymSystem.dao.CrudUtil;
+import GymSystem.dao.custom.AccessoriesDAO;
 import GymSystem.entity.Accessories;
+import GymSystem.entity.Member;
 
-public interface AccessoriesDAO extends CrudDAO<Accessories, String > {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AccessoriesDAOImpl implements AccessoriesDAO {
+    @Override
+    public void CrudTest() {
+
+    }
+
+    @Override
+    public boolean add(Accessories ref) throws ClassNotFoundException, SQLException {
+
+        return CrudUtil.executeUpdate("Insert into MEMBER values(?,?,?,?)", ref.getId(), ref.getType(), ref.getBrand(), ref.getQty());
+    }
+
+    @Override
+    public boolean update(Accessories ref) throws ClassNotFoundException, SQLException {
+        return CrudUtil.executeUpdate("update MEMBER set AID=?,Atype=?,Abrand=?,AQuan=? where AID=?",  ref.getType(), ref.getBrand(), ref.getQty());
+    }
+
+    @Override
+    public boolean delete(String id) throws ClassNotFoundException, SQLException {
+        return CrudUtil.executeUpdate("delete from ACCESSORY where AID=?", id);
+    }
+
+    @Override
+    public Accessories search1(String id) throws ClassNotFoundException, SQLException {
+        ResultSet rst = CrudUtil.executeQuery("select * from ACCESSORY where AID=?", id);
+        if (rst.next()){
+            return new Accessories(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4),
+
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public Accessories search2(String t) throws ClassNotFoundException, SQLException {
+        return null;
+    }
+
+
+    @Override
+    public ArrayList<Accessories> getAll() throws Exception {
+        ArrayList<Accessories> company = new ArrayList<>();
+        ResultSet rst = CrudUtil.executeQuery("Select * from ACCESSORY");
+        while (rst.next()){
+            company.add(new Accessories(
+                    rst.getString(1),
+                    rst.getString(2),
+                    rst.getString(3),
+                    rst.getString(4),
+
+            ));
+        }
+        return company;
+    }
 
 }
+
