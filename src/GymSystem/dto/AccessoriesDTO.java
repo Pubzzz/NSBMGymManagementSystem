f3738f4bb4f044fe9628105fc4fca51e928d5128
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2020-11-16 23:15:41 +0530

    Update AccessoriesDTO.java

--- src/GymSystem/dto/AccessoriesDTO.java
+++ src/GymSystem/dto/AccessoriesDTO.java
@@ -1,5 +1,52 @@
 package GymSystem.dto;
 
 public class AccessoriesDTO {
+    private String id ;
+    private String type  ;
+    private String brand ;
+    private String qty ;
+
+    public AccessoriesDTO(){
+
+    }
+    public AccessoriesDTO(String id,String type,String brand,String qty){
+        this.id = id;
+        this.type = type;
+        this.brand = brand;
+        this.qty = qty;
+
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String tel) {
+        this.qty = qty;
+    }
+
 
 }
