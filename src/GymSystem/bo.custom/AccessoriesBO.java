c4f292a7d609a6f8664a3fbe15228091d87f4ea1
Malith H Puhulwellage <56906043+malithsumuditha@users.noreply.github.com>
2020-11-16 23:09:50 +0530

    Update AccessoriesBO.java

--- src/GymSystem/bo.custom/AccessoriesBO.java
+++ src/GymSystem/bo.custom/AccessoriesBO.java
@@ -1,4 +1,17 @@
 package GymSystem.bo.custom;
 
 public interface AccessoriesBO {
+  public  boolean addAccessory(AccessoriesDTO ref) throws ClassNotFoundException, SQLException;
+
+    public  boolean updateAccessory(AccessoriesDTO ref) throws SQLException, ClassNotFoundException ;
+
+    public  boolean removeAccessory(String id) throws SQLException, ClassNotFoundException ;
+
+    public  MemberDTO searchAccessory(String id) throws SQLException, ClassNotFoundException;
+
+    public ArrayList<MemberDTO> getAllAccessory()throws Exception;
+
+    boolean addAccessory(AccessoriesDTO ref);
+
+    boolean updateAccessory(AccessoriesDTO ref);
 }
