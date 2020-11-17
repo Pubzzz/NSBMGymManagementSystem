41f6a15e75491726ac0b2ecaed2bd3b76e355576
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-11-17 10:34:46 +0530

    Update LoginViewController.java

--- src/GymSystem/controller/LoginViewController.java
+++ src/GymSystem/controller/LoginViewController.java
@@ -1,6 +1,7 @@
 package GymSystem.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,35 +9,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginViewController {
+    @FXML
+    private JFXButton btn_Login;
 
     @FXML
-    private TextField txt_username;
+    private JFXTextField txt_Username;
 
     @FXML
-    private TextField txt_password;
+    private JFXPasswordField txt_Password;
 
-    @FXML
-    private Button btn_Login;
-
-
-
-    public void login_action(ActionEvent actionEvent) throws IOException {
-
-
-        String text = txt_username.getText();
-        String password=txt_password.getText();
+    public void onaction_Login(ActionEvent actionEvent) throws IOException {
+        String text = txt_Username.getText();
+        String password=txt_Password.getText();
 
 
 
-        if(text.equalsIgnoreCase("") && password.equalsIgnoreCase("")){
+        if(text.equalsIgnoreCase("1") && password.equalsIgnoreCase("1")){
             Parent parent= FXMLLoader.load(this.getClass().getResource("/GymSystem/view/DashboardMainView.fxml"));
             Scene scene=new Scene(parent);
             Stage stage = (Stage) this.btn_Login.getScene().getWindow();
@@ -45,10 +39,18 @@ public class LoginViewController {
             stage.show();
 
         }else
-            {
+        {
             Alert a = new Alert(Alert.AlertType.WARNING, "Invalid Login ", ButtonType.OK);
             a.showAndWait();
         }
     }
+
+    public void onaction_Username(ActionEvent actionEvent) {
+        txt_Username.requestFocus();
     }
+
+    public void onaction_Password(ActionEvent actionEvent) {
+        txt_Password.requestFocus();
+    }
+}
 
