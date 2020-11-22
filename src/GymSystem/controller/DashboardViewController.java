1ca0de7b822b5f8ba52b99a658cf7e5aa2248621
Dilan Ranawaka <47558301+DilanRanawaka@users.noreply.github.com>
2020-11-22 20:39:53 +0530

    Create DashboardViewController.java

---
+++ src/GymSystem/controller/DashboardViewController.java
@@ -0,0 +1,32 @@
+package GymSystem.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardViewController {
+
+
+
+    @FXML
+    private JFXButton btn_ViewMember;
+
+    @FXML
+    private JFXButton btn_Viewattendance;
+
+    @FXML
+    private JFXButton btn_Viewinstructor;
+
+
+    public void onaction_ViewMember(ActionEvent actionEvent)  {
+
+    }
+
+    public void onaction_Viewattendance(ActionEvent actionEvent) {
+
+    }
+
+    public void onaction_Viewinstructor(ActionEvent actionEvent) {
+
+    }
+}
